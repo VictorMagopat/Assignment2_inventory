64b758c0367a7cb0fc6b8247bb51781f4aea7530
VictorMagopat <magopat.victor@gmail.com>
2023-10-23 16:25:20 -0400

    12 month loop
    created a separate .py file to hold the class information.
    added basic comments.
    added the ability to calculate revenue, cost and profit.
    made a 12 month loop that simulates business and shows profit.

--- inventory.py
+++ inventory.py
@@ -1,8 +1,9 @@
 # this is inventory.py file. happy
+# import all moduals
+import random
+import product
 
-
-print("Welcome to Programming Principles Sample Product Inventory")
-
+# Determine if the variable can be turned into a Int or Float.
 def Is_Number(number):
     num_truth = number.isnumeric()
     if num_truth == False:
@@ -13,83 +14,124 @@ def Is_Number(number):
             num_truth = False
     return num_truth
 
+# sets a name of the product to the object
+def Enter_Product_Name():
+    Product_Name = input("Please enter the Product Name: ")
+    Chair.set_Name(Product_Name)
 
-
-class Product:
-    def __init__(self, Product_Name,Curent_Stock, Sale_Price, Manufacture_Cost, Monthly_Production):
-        pass
-
-    def set_name_product(Product):
-        Product_Name = input("Please enter the Product Name: ")
-        Product.Product_Name = Product_Name
-
-    def set_Stock(Product):
-        while True:
-            Curent_Stock = input("Please enter the current amount in stock: ")
-            Number_truth = Is_Number(Curent_Stock)
-            if Number_truth == True:
-                Product.Curent_Stock = int(Curent_Stock)
+# sets a code associated to the product to the object
+def Enter_Code():
+    while True:
+        product_code = input("Please enter the code of the product [100 to 1000]: ")
+        valid_number = Is_Number(product_code)
+        if valid_number == True:
+            product_code = int(product_code)
+            if product_code <= 1000 and product_code >= 100: 
+                Chair.set_Code(int(product_code))
                 return None
-            else: print("please input a proper number.")
+            else: print("Please enter a number in range 100 to 1000.")
+        else: print("Please enter a number in range 100 to 1000.")
 
-    def set_Sale_Price(Product):
-        while True:
-            Sale_Price = input("Please enter the products price: ")
-            Number_truth = Is_Number(Sale_Price)
-            if Number_truth == True:
-                Product.Sale_Price = float(Sale_Price)
-                return None
-            else: print("please input a proper number.")
+# sets the curent stock of the product to the object
+def Enter_Stock():
+    while True:
+        curent_stock = input("Please enter the current count in stock: ")
+        valid_number = Is_Number(curent_stock)
+        if valid_number == True:
+            Chair.set_Stock(int(curent_stock))
+            return None
+        else: print("Please input a proper number.")
 
-    def set_Manufacture_Cost(Product):
-        while True:
-            Manufacture_Cost = input("Please enter the cost of manufacturing the product: ")
-            Number_truth = Is_Number(Manufacture_Cost)
-            if Number_truth == True:
-                Product.Manufacture_Cost = float(Manufacture_Cost)
-                return None
-            else: print("please input a proper number.")
+# sets the sale price of the product to the object
+def Enter_Sale_Price():
+    while True:
+        sale_price = input("Please enter the products selling price: ")
+        valid_number = Is_Number(sale_price)
+        if valid_number == True:
+            Chair.set_Sale_Price(float(sale_price))
+            return None
+        else: print("Please input a proper number.")
 
-    def set_Monthly_Production(Product):
-        while True:
-            Monthly_Production = input("Please enter the amount of product made in one month: ")
-            Number_truth = Is_Number(Monthly_Production)
-            if Number_truth == True:
-                Product.Monthly_Production = int(Monthly_Production)
-                return None
-            else: print("please input a proper number.")
+# sets the cost to make the product to the object
+def Enter_Manufacture_Cost():
+    while True:
+        manufacture_cost = input("Please enter the cost of manufacturing the product: ")
+        valid_number = Is_Number(manufacture_cost)
+        if valid_number == True:
+            Chair.set_Manufacture_Cost(float(manufacture_cost))
+            return None
+        else: print("Please enter a proper number.")
+
+# sets the amount stock made  of the product to the object
+def Enter_Monthly_Production():
+    while True:
+        monthly_production = input("Please enter the amount of product made in one month: ")
+        valid_number = Is_Number(monthly_production)
+        if valid_number == True:
+            Chair.set_Monthly_Production(int(monthly_production))
+            return None
+        else: print("Please input a proper number.")
+
+# simulates the amount of product sold in a month.
+def Stock_Sold(current_stock):
+    number_sold = random.randrange(0, current_stock)
+    return number_sold
+
+# calculates the stock remaining after a month of selling the product.
+def Find_Stock_Remaining(current_stock, number_sold):
+    remaining_stock = current_stock - number_sold
+    return remaining_stock
+
+# calculates the stock after the month of production.
+def Stock_Production(current_stock, stock_produced):
+    total_stock = current_stock + stock_produced
+    return total_stock
+
+# calculates the cost of producing more products for every month
+def Cost_Of_Production(monthly_production, manufacture_cost):
+    monthly_cost = monthly_production * manufacture_cost
+    return monthly_cost
+
+# calculates the revenue for the month, subtracts the cost and then adds the previos months profit
+def Total_Profit(number_sold, sale_price, monthly_cost, profit):
+    revenue = number_sold * sale_price
+    profit = revenue - monthly_cost + profit
+    return profit
+
+Chair = product.Product()
+
+Enter_Product_Name()
+Enter_Code()
+Enter_Stock()
+Enter_Sale_Price()
+Enter_Manufacture_Cost()
+Enter_Monthly_Production()
 
 
+print("****** Programming Principles Sample Stock Statement******")
+print(" Product Code:", Chair.Code)
+print(" Product Name: ", Chair.Name)
 
-Chair = Product
-
-Chair.set_name_product(Chair)
-Chair.set_Stock(Chair)
-Chair.set_Sale_Price(Chair)
-Chair.set_Manufacture_Cost(Chair)
-Chair.set_Monthly_Production(Chair)
-
-print(Chair.Product_Name)
-print(Chair.Curent_Stock)
-print(Chair.Sale_Price)
-print(Chair.Manufacture_Cost)
-print(Chair.Monthly_Production)
+print(" Sale Price: ", Chair.Sale_Price)
+print(" Manufacture Cost :", Chair.Manufacture_Cost)
+print(" Monthly Production: ", Chair.Monthly_Production)
+print("Welcome to Programming Principles Sample Product Inventory")
 
 
-
-
-month_count = 13
+profit = 0
+month_count = 1
 while month_count <= 12:
-    print("****** Programming Principles Sample Stock Statement******")
-    print(" Product Code:")
-    print(" Product Name: ", Product_Name)
-
-    print(" Sale Price: ", Sale_Price)
-    print(" Manufacture Cost :", Manufacture_Cost)
-    print(" Monthly Production: ", Monthly_Production)
-
+    print("____________________________________________")
     print(" Month:", month_count)
-    print(" Manufactured: 100 units")
-    print(" Sold: 110 units")
-    print(" Stock: 90 units")
+    print(" Manufactured: " + str(Chair.Monthly_Production) + " units")
+    sold = Stock_Sold(Chair.Stock)
+    print(" Sold: "+ str(sold) + " units")
+    Chair.Stock = Find_Stock_Remaining(Chair.Stock, sold)
+    Chair.Stock = Stock_Production(Chair.Stock, Chair.Monthly_Production)
+    print(" Stock: "+ str(Chair.Stock) + " units")
+    monthly_cost = Cost_Of_Production(Chair.Monthly_Production, Chair.Manufacture_Cost)
+    profit = Total_Profit(sold, Chair.Sale_Price, monthly_cost, profit)
     month_count += 1
+
+
+print("the total profit was: ", profit)
---
+++ product.py
@@ -0,0 +1,37 @@
+# this is product.py file. 
+
+
+class Product:
+# all the variabls that are attached to the product
+    def __init__(self, Name = "Unknown", Code = 100, Stock = 0, Sale_Price = 0, Manufacture_Cost = 0, Monthly_Production = 0):
+        self.Name = Name
+        self.Code = Code
+        self.Stock = Stock
+        self.Sale_Price = Sale_Price
+        self.Manufacture_Cost = Manufacture_Cost
+        self.Monthly_Production = Monthly_Production
+        pass
+
+# built in function that changes the Name.
+    def set_Name(self, NewName):        
+        self.Name = NewName
+
+# built in function that changes the Code.
+    def set_Code(self, NewCode):
+        self.Code = NewCode
+
+# built in function that changes the current stock
+    def set_Stock(self, NewStock):
+        self.Stock = NewStock
+
+# built in function that changes the sale price of the product
+    def set_Sale_Price(self, NewSalePrice):
+        self.Sale_Price = NewSalePrice
+
+# built in function that changes the cost of manufacturing the product
+    def set_Manufacture_Cost(self, NewManufactureCost):
+        self.Manufacture_Cost = NewManufactureCost
+
+# built in function that changes the total amount of product made that month
+    def set_Monthly_Production(self, NewMonthlyProduction):
+        self.Monthly_Production = NewMonthlyProduction
